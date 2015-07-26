6bb1cb9d67c841c39b83e9ca1244699c62ec440d
orcacvsmirror <orcacvsmirror@gmail.com>
2015-07-27 04:04:52 +0900

    Modified logging for import-all.sh

--- import-all.sh
+++ import-all.sh
@@ -4,7 +4,5 @@ ROOT_DIR=$(cd $(dirname $0)/.. && pwd)
 CVS_MODULES=(jma-receipt jma-receipt-kk jma-receipt-forms ikensyo tokutei qkan pims)
 
 for module in ${CVS_MODULES[*]}; do
-    echo "importing $module ..."
-    $ROOT_DIR/cvsimporter/import.sh $module
-    echo "imported $module"
+    $ROOT_DIR/cvsimporter/import.sh $module >/dev/null 2>&1
 done
